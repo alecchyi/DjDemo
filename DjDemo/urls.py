d38fe1acf2a997500bdb4f9f07899e26fc515c71
alecchyi <alecchyi@gmail.com>
2014-01-16 15:10:33 +0800

    add login code

--- DjDemo/urls.py
+++ DjDemo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','core.views.index'),
+    url(r'^login', 'core.view.login'),
+    
 )
--- core/views.py
+++ core/views.py
@@ -6,6 +6,10 @@ def index(request):
 
     return render(request,"index.html")
 
+def login(request):
+    
+    return render(request,"login.html")
+
 def global_context(request):
     context = {'is_login':False}
     return context
