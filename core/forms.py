71c801b6a6e85e367c37245620677fefa2a60681
alecchyi <alecchyi@gmail.com>
2014-01-21 13:16:45 +0800

    add max_length for username

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,7 @@ all forms
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
+    username = forms.CharField(max_length=50)
     password = forms.CharField(max_length=50, widget=forms.PasswordInput)
     confirm = forms.CharField(max_length=50, widget = forms.PasswordInput)
     
